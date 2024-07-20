b17e5dd47c40aad6677d5853f17019d1243ba29a
aroun0 <kirkorn@ya.ru>
2024-07-21 00:09:37 +0300

    Add function Webrequest

--- check-url.ps1
+++ check-url.ps1
@@ -1,7 +1,16 @@
 ﻿$url="google.com"
 
+
+
+
+
+
+func WebRequest {
+
 $validation = Invoke-WebRequest -Uri $url -UseDefaultCredentials -UseBasicParsing -Method Head -TimeoutSec 5 -ErrorAction Stop
 $status = [int]$validation.StatusCode
-
-
 Write-Output $status
+
+}
+
+WebRequest
